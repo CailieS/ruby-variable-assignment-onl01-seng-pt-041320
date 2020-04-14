3f489321a7de19f422b80882e80a0e36929bd0bd
Cailie Skelton <cskelton707@gmail.com>
2020-04-14 22:25:31 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,4 @@
  # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-
-greeting = "Hello World"
-this is ta test #{greeting}
+ # give it the value of the string "Hello Worlld"
+ 
+ greeting = "Hello World"
